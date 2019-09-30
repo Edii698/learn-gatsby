edf1e025b0234693974b8dc13e04004b22009159
Eddie <e.otero81@gmail.com>
2019-09-30 06:42:39 -0400

    added layouts

--- hello-world/src/pages/index.js
+++ hello-world/src/pages/index.js
@@ -1,29 +1,20 @@
 import React from "react"
-import Header from "../components/Header"
-import Footer from "../components/Footer"
+import PrimaryLayout from "../layouts/PrimaryLayout"
 import Post from "../components/Post"
 
 export default () => (
-  <div>
-    <Header></Header>
-    <div className="container">
-      <div className="row justify-content-md-center">
-        <div className="col-xs-6">
-          <Post
-            title="This is our first post"
-            excerpt="We are writing some place holder text here"
-          ></Post>
-          <Post
-            title="This is our first post"
-            excerpt="We are writing some place holder text here"
-          ></Post>
-          <Post
-            title="This is our first post"
-            excerpt="We are writing some place holder text here"
-          ></Post>
-        </div>
-      </div>
-    </div>
-    <Footer></Footer>
-  </div>
+  <PrimaryLayout col="col-10">
+    <Post
+      title="This is our first post"
+      excerpt="We are writing some place holder text here"
+    ></Post>
+    <Post
+      title="This is our first post"
+      excerpt="We are writing some place holder text here"
+    ></Post>
+    <Post
+      title="This is our first post"
+      excerpt="We are writing some place holder text here"
+    ></Post>
+  </PrimaryLayout>
 )
